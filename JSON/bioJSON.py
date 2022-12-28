0f2ed075b191c3b2391ce9eefc3740f7f3b552f7
Mumble <lydiakahouadji06@gmail.com>
2022-12-28 23:26:21 +0100

    json spliting

--- JSON/bioJSON.py
+++ JSON/bioJSON.py
@@ -1,22 +1,12 @@
 import json
 
-try:
-  with open('litcovid2BioCJSON.json', 'r') as f:
-    data = json.load(f)
-except FileNotFoundError:
-  print("Error: Input file not found")
-  exit()
-except json.decoder.JSONDecodeError:
-  print("Error: Input file is not a valid JSON file")
-  exit()
+with open('litcovid2BioCJSON.json', 'r') as f:
+  data = json.load(f)
 
-array = data[1]
+  array = data[1]
 
-for i, element in enumerate(array):
-  try:
+  for i, element in enumerate(array):
     with open('document{}.json'.format(i), 'w') as f:
       json.dump(element, f)
       f.write(json.dumps(element))
       print("created" + i)
-  except IOError:
-    print(f"Error: Could not write to output file element{i}.json")
